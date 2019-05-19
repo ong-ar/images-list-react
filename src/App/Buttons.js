92517901e0fef477b5f41646ab268c9d55307966
ong-ar <reclusis@gmail.com>
2019-05-19 20:53:21 +0900

    리덕스 액션 버튼 적용

--- src/App/Buttons.js
+++ src/App/Buttons.js
@@ -1,14 +1,30 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { actionCreators as dogsActions } from "../redux/modules/dogs";
 
 class Buttons extends Component {
   render() {
-    return(
+    return (
       <div>
-        <button>GET DOGS</button>
-        <button>CLEAR DOGS</button>
+        <button onClick={this.props.getDogs}>GET DOGS</button>
+        <button onClick={this.props.clearDogs}>CLEAR DOGS</button>
       </div>
     );
   }
 }
 
-export default Buttons;
+const mapActionToProps = dispatch => {
+  return {
+    getDogs: () => {
+      dispatch(dogsActions.getDogs());
+    },
+    clearDogs: () => {
+      dispatch(dogsActions.clearDogs());
+    }
+  };
+};
+
+export default connect(
+  null,
+  mapActionToProps
+)(Buttons);
