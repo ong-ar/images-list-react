b253cdc627a975a4ba4ad097c5dacd0981d8f818
ong-ar <reclusis@gmail.com>
2019-05-21 16:58:23 +0900

    삭제 패키지 관련 소스 제거

--- src/App/List/index.js
+++ src/App/List/index.js
@@ -1,32 +1,19 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import Masonry from "react-masonry-component";
 import { actionCreators as dogsActions } from "../../redux/modules/dogs";
-import BottomScrollListener from "react-bottom-scroll-listener";
 import "./index.scss";
 
 class List extends Component {
-  onBottom = () => {
-    if (this.props.dogs.images.length > 0) {
-      this.props.getDogs();
-    }
-  };
   render() {
     return (
-      <BottomScrollListener onBottom={this.onBottom}>
-        <Masonry
+        <div
           className="Container"
-          options={{
-            horizontalOrder: true, // 왼쪽 -> 오른쪽으로 정렬
-            transitionDuration: 0 // 애니메이션 없도록
-          }}
           enableResizableChildren={false}
         >
           {this.props.dogs.images.map((image, index) => {
             return <img src={image} alt={index} key={index} />;
           })}
-        </Masonry>
-      </BottomScrollListener>
+        </div>
     );
   }
 }
