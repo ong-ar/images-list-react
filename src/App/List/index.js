d5c2cbbfd6f09efb10c1e731b3ea304e4e7f3746
ong-ar <reclusis@gmail.com>
2019-05-22 16:47:25 +0900

    css injection

--- src/App/List/index.js
+++ src/App/List/index.js
@@ -4,16 +4,37 @@ import { actionCreators as dogsActions } from "../../redux/modules/dogs";
 import "./index.scss";
 
 class List extends Component {
+  componentDidUpdate() {
+    const parentElement = document.querySelector('#Container');
+    console.log(parentElement);
+    const childrenElements = parentElement.children;
+    console.log(childrenElements);
+    for(let i = 0; i < childrenElements.length; i++) {
+      const element = childrenElements[i];
+      
+      const css = element.style;
+      css['width'] = "200px";
+      css['height'] = "200px";
+      css['position'] = "absolute";
+      if(i > 0) {
+        css['left'] = childrenElements[i-1].offsetLeft + 200 + 'px';
+      }
+      console.log(css);
+    }
+    
+    
+  }
   render() {
     return (
-        <div
-          className="Container"
-          enableResizableChildren={false}
-        >
-          {this.props.dogs.images.map((image, index) => {
-            return <img src={image} alt={index} key={index} />;
-          })}
-        </div>
+      <div className="Container" id='Container'>
+        {this.props.dogs.images.map((image, index) => {
+          return (
+            <div key={index}>
+              <img src={image} alt={index}  />
+            </div>
+          );
+        })}
+      </div>
     );
   }
 }
