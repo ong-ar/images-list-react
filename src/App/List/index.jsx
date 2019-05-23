77622a4cb72d59b2f51e101babe6b54b9e348041
ong-ar <reclusis@gmail.com>
2019-05-23 19:01:31 +0900

    list css #1

--- src/App/List/index.jsx
+++ src/App/List/index.jsx
@@ -6,12 +6,50 @@ import './index.scss';
 
 
 class List extends PureComponent {
+  componentDidUpdate() {
+    const parentElement = document.querySelector('#Container');
+    const parentOffsetWidth = parentElement.offsetWidth;
+    console.log(parentOffsetWidth);
+    console.log(parentElement);
+    const childrenElements = parentElement.children;
+    console.log(childrenElements);
+    let firstNewlineIndex = -1;
+    for (let i = 0; i < childrenElements.length; i += 1) {
+      const element = childrenElements[i];
+
+      const css = element.style;
+      css.width = '200px';
+      css.height = 'auto';
+      css.position = 'absolute';
+      if (i === 0) {
+        css.left = '0px';
+      } else {
+        if (parentOffsetWidth < childrenElements[i - 1].offsetLeft + 200) {
+          if (firstNewlineIndex === -1) {
+            firstNewlineIndex = i + 1;
+          }
+          css.left = '0px';
+        } else {
+          css.left = `${childrenElements[i - 1].offsetLeft + 200}px`;
+          if (firstNewlineIndex !== -1) {
+            css.top = childrenElements[i - firstNewlineIndex].offsetTop
+             + childrenElements[i - firstNewlineIndex].offsetHeight;
+            console.log(childrenElements[i - firstNewlineIndex].top);
+          } else {
+            css.top = '0px';
+          }
+        }
+        console.log(css);
+      }
+    }
+  }
+
   render() {
     const { dogs } = this.props;
     return (
       <div
         className="Container"
-        enableResizableChildren={false}
+        id="Container"
       >
         {dogs.images.map((image, index) => <img src={image} alt={index} key={image} />)}
       </div>
