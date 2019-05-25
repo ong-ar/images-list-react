6c8bea33062f5c14e2d6a200638174138145c668
ong-ar <reclusis@gmail.com>
2019-05-25 22:53:23 +0900

    handleOnLoad 부모 엘리먼트 높이 계산 추가

--- src/App/List/index.jsx
+++ src/App/List/index.jsx
@@ -6,15 +6,17 @@ import _ from 'lodash';
 import './index.scss';
 
 class List extends PureComponent {
+  list = null;
+
   // img 버블링이 불가능하여 img onLoad
   // onload 시 모두 실행되지 않도록 debounce 적용
-  static sortImages = _.debounce(() => {
-    console.log('sort images call');
-    const parentElement = document.querySelector('#Container');
+  handleOnLoad = _.debounce(() => {
+    const parentElement = this.list;
     const parentOffsetWidth = parentElement.offsetWidth;
     const childrenElements = parentElement.children;
     let firstNewlineIndex = -1; // 한 줄에 몇개의 컬럼이 있는지 담는 변수
     let columnsInfo = []; // masonry 정렬을 위해 필요한 변수
+    let parentHeight = 0;
 
     // children 엘리먼트 전부 한번씩 접근
     for (let i = 0; i < childrenElements.length; i += 1) {
@@ -25,22 +27,27 @@ class List extends PureComponent {
       // 이미지 모두 포지션을 absolute
       css.position = 'absolute';
 
+      // top, left 변수 선언
+      let top = 0;
+      let left = 0;
+
+      // top, left 계산
       // 가장 첫번째 이미지인 경우 위치를 top, left : 0
       if (i === 0) {
-        css.top = '0px';
-        css.left = '0px';
+        top = 0;
+        left = 0;
       } else {
         // 부모 엘리먼트 width 를 넘어가는 순간 column 갯수 저장
         if (parentOffsetWidth < childrenElements[i - 1].offsetLeft
           + childrenElements[i - 1].offsetWidth
-          + childrenElements[i].offsetWidth && firstNewlineIndex === -1) {
+          + element.offsetWidth && firstNewlineIndex === -1) {
           firstNewlineIndex = i;
         }
 
         // column 갯수가 아직 저장되지 않는 경우 top: 0 고정, left 는 바로 왼쪽 element width 값을 받아 left
         if (firstNewlineIndex === -1) {
-          css.top = '0px';
-          css.left = `${childrenElements[i - 1].offsetLeft + childrenElements[i - 1].offsetWidth}px`;
+          top = 0;
+          left = childrenElements[i - 1].offsetLeft + childrenElements[i - 1].offsetWidth;
         } else {
           // element 가 개행 후 첫번째 일 때 그 행에 있는 column 들의 top, left 계산
           if (i % firstNewlineIndex === 0) {
@@ -65,20 +72,33 @@ class List extends PureComponent {
               });
           }
           // 계산 된 top, left 적용
-          css.top = `${columnsInfo[i % firstNewlineIndex].top}px`;
 
-          css.left = `${columnsInfo[i % firstNewlineIndex].left}px`;
+          const applyingIndex = i % firstNewlineIndex;
+          const { top: applyingTop, left: applyingLeft } = columnsInfo[applyingIndex];
+
+          top = applyingTop;
+          left = applyingLeft;
         }
       }
+
+      // top, left 적용
+      css.top = `${top}px`;
+      css.left = `${left}px`;
+
+      // 부모 엘리먼트 height 계산
+      parentHeight = top + element.offsetHeight > parentHeight
+        ? top + element.offsetHeight : parentHeight;
     }
+    parentElement.style.height = `${parentHeight}px`;
   }, 100);
+
 
   render() {
     const { dogs } = this.props;
     return (
       <div
-        className="Container"
-        id="Container"
+        className="List"
+        ref={(ref) => { this.list = ref; }}
       >
         {dogs.images.map((image, index) => {
           // key 로 index 값을 사용하지 못하여 임시로 재정의하여 사용
@@ -88,7 +108,7 @@ class List extends PureComponent {
           // onload 는 버블링이 되지 않아 이미지 이벤트에 정렬 적용
           return (
             <div>
-              <img src={image} alt={index} key={key} onLoad={List.sortImages} />
+              <img src={image} alt={index} key={key} onLoad={this.handleOnLoad} />
             </div>
           );
         })}
